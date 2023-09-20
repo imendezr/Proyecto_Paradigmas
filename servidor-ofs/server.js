24ef5b54c8c85d74deaae326e3aa27d2931f635a
imendezr <90487354+imendezr@users.noreply.github.com>
2023-09-20 16:10:28 -0600

    client-server fixed

--- servidor-ofs/server.js
+++ servidor-ofs/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const cors = require('cors'); // Cross-Origin Resource Sharing
 const app = express();
 const PORT = 3001;
+
+// Configura CORS
+app.use(cors({
+  origin: 'http://localhost:3000', // Permite las solicitudes desde este origen
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+
+app.use(express.json()); // Añade el middleware para parsear JSON
 
 app.get('/', (req, res) => {
   res.send('Servidor OFS en funcionamiento!');
