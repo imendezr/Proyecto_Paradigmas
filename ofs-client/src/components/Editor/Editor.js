f8050e23c787f1449f373bc465f474a41710494c
DiegexUNA <diego.sanchez.jimenez@est.una.ac.cr>
2023-09-26 22:21:44 -0600

    Predictive text

--- ofs-client/src/components/Editor/Editor.js
+++ ofs-client/src/components/Editor/Editor.js
@@ -26,13 +26,37 @@ import {retrieveScript, saveScript} from '../../api/scripts';
 
 const Editor = ({code, setStatusBarMessage, onCodeChange}) => {
     const [id, setId] = useState('');
-
+    const [suggestions, setSuggestions] = useState([]);
     /**
      ### Guardar Script
 
      Esta función guarda el código actual bajo el nombre especificado y muestra un mensaje en la barra de estado.
      */
+    const predefinedWords = ["if", "else", "for", "while", "function", "const", "let", "var", "return"];
 
+// Implementa la función para obtener sugerencias basadas en el texto de entrada.
+    const getSuggestionsBasedOnInput = (inputText) => {
+        inputText = inputText.toLowerCase(); // Convierte el texto de entrada a minúsculas para hacer coincidencias sin distinción entre mayúsculas y minúsculas.
+
+        // Filtra las palabras predefinidas que comienzan con el texto de entrada.
+        const filteredSuggestions = predefinedWords.filter((word) => {
+            return word.toLowerCase().startsWith(inputText);
+        });
+
+        return filteredSuggestions;
+    };
+    const handleSuggestions = (inputText) => {
+        // Aquí puedes implementar la lógica para obtener sugerencias basadas en inputText.
+        // Por ejemplo, puedes buscar coincidencias en una lista de palabras predefinidas.
+        // Luego, actualiza el estado de las sugerencias.
+        const suggestionsList = getSuggestionsBasedOnInput(inputText); // Implementa esta función.
+        setSuggestions(suggestionsList);
+    };
+    const handleSuggestionClick = (suggestion) => {
+        // Manejar la selección de sugerencias y actualizar el texto en el área de edición.
+        onCodeChange(suggestion);
+        setSuggestions([]); // Limpiar la lista de sugerencias
+    };
     const handleSaveScript = async () => {
         !id ? setStatusBarMessage("Error: ingrese un nombre para el script.") : (async () => {
             const result = await saveScript(id, code);
@@ -68,10 +92,20 @@ const Editor = ({code, setStatusBarMessage, onCodeChange}) => {
             </div>
             <textarea
                 value={code}
-                onChange={(e) => onCodeChange(e.target.value)}
+                onChange={(e) => {
+                    onCodeChange(e.target.value);
+                    handleSuggestions(e.target.value);
+                }}
                 rows="10"
                 cols="50"
             ></textarea>
+            <ul>
+                {suggestions.map((suggestion, index) => (
+                    <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
+                        {suggestion}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
