30e130ddfd12a0a586cdd1b71efb66081a0524ad
fabio0610 <fabio0610@hotmail.com>
2023-09-27 01:24:37 -0600

    Se agregan saltos de linea en TA

--- ofs-client/src/components/Output/Output.js
+++ ofs-client/src/components/Output/Output.js
@@ -12,12 +12,62 @@
  Se utiliza un área de texto de solo lectura para mostrar el resultado.
  */
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-const Output = ({result}) => {
+
+const Output = ({ result }) => {
+    const textareaRef = useRef(null);
+    const lineNumbersRef = useRef(null);
+
+    const handleScroll = () => {
+        const textarea = textareaRef.current;
+        const lineNumbers = lineNumbersRef.current;
+        lineNumbers.scrollTop = textarea.scrollTop;
+    };
+
+    const getLineNumbers = () => {
+        const lines = result.split('\n');
+        const totalLines = lines.length;
+        const lineNumbers = [];
+        for (let i = 1; i <= totalLines; i++) {
+            lineNumbers.push(<div key={i} className="line-number">{i}</div>);
+        }
+        return lineNumbers;
+    };
+
+    useEffect(() => {
+        const textarea = textareaRef.current;
+        const lineNumbers = lineNumbersRef.current;
+
+        const updateLineNumbersHeight = () => {
+            const textareaComputedStyle = getComputedStyle(textarea);
+            lineNumbers.style.height = textareaComputedStyle.height;
+        };
+
+        updateLineNumbersHeight();
+
+        textarea.addEventListener('input', updateLineNumbersHeight);
+        textarea.addEventListener('scroll', handleScroll);
+
+        return () => {
+            textarea.removeEventListener('input', updateLineNumbersHeight);
+            textarea.removeEventListener('scroll', handleScroll);
+        };
+    }, [result]);
+
     return (
         <div className="TA">
-            <textarea readOnly value={result} rows="10" cols="50"></textarea>
+            <div className="line-numbersTA" ref={lineNumbersRef}>
+                {getLineNumbers()}
+            </div>
+            <textarea
+                ref={textareaRef}
+                readOnly
+                value={result}
+                onScroll={handleScroll}
+                rows="10"
+                cols="50"
+            ></textarea>
         </div>
     );
 };
